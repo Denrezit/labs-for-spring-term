480a6f228553e659783f39c5f8910f592f96853a
Anastasia Savitskaya <112703292+Denrezit@users.noreply.github.com>
2025-03-07 21:10:03 +1000

    Update task-std.cpp

--- code for 4 lab/task-std/task-std/task-std.cpp
+++ code for 4 lab/task-std/task-std/task-std.cpp
@@ -1,4 +1,4 @@
-﻿// На вход подаётся строка из целого числа, нужно сделать так, чтобы эта строка
+// На вход подаётся строка из целого числа, нужно сделать так, чтобы эта строка
 // Отсортировалась по возрастанию цифр, если число меньше 1000.
 // В другом случае в строке нужно будет найти цифру 5, если такая имеется.
 // Затем выводится её первая позиция в строке,
@@ -19,20 +19,21 @@ int main()
     std::cout << "Введите любое целое число: ";
     std::cin >> str_number;
 
-    integer_number = std::stoi(str_number);
+    integer_number = std::stoi(str_number); // Преобразование в целочисленный тип
     if (integer_number < 1000) {
-        sort(str_number.begin(), str_number.end());
+        sort(str_number.begin(), str_number.end()); // Сортировка по возрастанию
         sort_number = std::stoi(str_number);
-        std::cout << sort_number + integer_number;
+        std::cout << sort_number + integer_number; // Сумма отсортированного числа и изначального числа
     }
     else {
-        auto searching_num = std::find(str_number.begin(), str_number.end(), '5');
-        if (searching_num != str_number.end()) { //Если искомый элемент не равен элементу, следующим за последним элементом
-            std::cout << std::distance(str_number.begin(), searching_num); //Индекс элемента
+        auto searching_num = std::find(str_number.begin(), str_number.end(), '5'); // Поиск цифры 5 в строке
+        if (searching_num != str_number.end()) { // Если искомый элемент не равен элементу, следующим за последним элементом
+            std::cout << "Позиция цифры 5: " << std::distance(str_number.begin(), searching_num); // Индекс элемента
         }
         else {
             std::cout << "Цифра 5 не найдена.";
         }
     }
-
+    return 0;
 }
+
